71f00962870bd058fe81230e52f27aa2cb6eeb7e
adrianit248 <adrianit248@gmail.com>
2025-04-23 22:41:15 -0400

    started API integration to minute 12:30

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
 function Home() {
-    return <h1>Home</h1>
+    const [users, setUsers] = useState([])
+    async function fetchUsers() {
+        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+        setUsers(data)
+        console.log(data)
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetchUsers();         
+        }, 500);
+    }, [])
+
+    const pixels = "2px"
+
+    return (
+        <div>
+            {users.map((user) => (
+                <div key={user.id} style={{border: `${pixels} solid black`}}>
+                <div>{user?.id}</div>
+                <div>{user?.name}</div>
+                <div>{user?.email}</div>
+                <div>{user?.username}</div>
+            </div>
+            ))}
+        </div>
+    );
 }
 
 export default Home
